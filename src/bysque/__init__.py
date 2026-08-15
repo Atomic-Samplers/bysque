63decb972a29e2330e5740f88494fc6dfe8ce0a2
tomdemeyere <t.demeyere@icloud.com>
2026-08-15 14:19:34 +0200

    initial template changes

--- src/bysque/__init__.py
+++ src/bysque/__init__.py
@@ -1,7 +1,7 @@
-"""Initialize the `template` package."""
+"""Initialize the `bysque` package."""
 
 from __future__ import annotations
 
 from importlib.metadata import version
 
-__version__ = version("template")
+__version__ = version("bysque")
